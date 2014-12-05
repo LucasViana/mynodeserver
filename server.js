b267fcc898cac3c49f50075a0a780fb098aeda0e
Lucas E Viana <lucasesvi@gmail.com>
2014-12-04 22:41:12 -0200

    stop request

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/static'));
+app.get('/', function (req, res) {
+  res.sendfile('index.html');
+});
 
-app.listen(80);
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+});
